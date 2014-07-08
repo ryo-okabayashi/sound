26702ec26535e036c993c0cd4f83744fe6c02881
Ryo Okabayashi <ryo.okabayashi@gmail.com>
2014-07-08 16:10:01 +0900

    remake

---
+++ MySound.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sound
+{
+    public class MySound : WaveProvider
+    {
+        double L;
+        double R;
+        uint count;
+        Random random = new Random();
+
+        Gen.Sine sine = new Gen.Sine(440);
+        Gen.Play s1 = new Gen.Play(@"C:\Users\ryo\Desktop\test.wav");
+
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            for (int n = 0; n < sampleCount; )
+            {
+                if (count % (44100/4) == 0)
+                {
+                    s1.phase = random.Next(0, 88200);
+                }
+                L = s1.val() * 0.5;
+                R = s1.val() * 0.5;
+
+                buffer[n++ + offset] = (float)L;
+                buffer[n++ + offset] = (float)R;
+
+                count++;
+            }
+
+            return sampleCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using NAudio.Wave;
+using System;
+
+namespace Sound
+{
+    class Program
+    {
+        public static WaveOut waveOut;
+
+        static void Main(string[] args)
+        {
+            var sound = new MySound();
+            sound.SetWaveFormat(44100, 2);
+            waveOut = new WaveOut();
+            waveOut.Init(sound);
+            waveOut.Play();
+
+            ConsoleKeyInfo keyInfo;
+            bool loop = true;
+            while (loop)
+            {
+                keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.Q)
+                {
+                    waveOut.Stop();
+                    waveOut.Dispose();
+                    loop = false;
+                }
+            }
+        }
+    }
+}
